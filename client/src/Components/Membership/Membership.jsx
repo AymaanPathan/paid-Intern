98ff31dca466de48093bccfa29d4ea73515b77d7
aymaan_2003 <aymaanpathan5@gmail.com>
2024-08-11 12:37:11 +0530

    modified membership component

--- client/src/Components/Membership/Membership.jsx
+++ client/src/Components/Membership/Membership.jsx
@@ -2,9 +2,11 @@ function Membership() {
   return (
     <div className="p-6 mt-12 flex flex-col items-center justify-center h-full text-center">
       <div>
-        <p className="font-bold text-sm">Subscribe</p>
-        <h3 className="text-4xl font-bold mt-2">All Access Membership</h3>
-        <span className="block mt-4 w-full">
+        <p className="font-bold text-3xl">Subscribe</p>
+        <h3 className="text-4xl font-bold mt-2 text-gray-400">
+          All Access Membership
+        </h3>
+        <span className="block mt-4 w-96 lg:text-md text-xs">
           Get unlimited access to all courses, exclusive content, and
           personalized learning paths, empowering you to achieve your goals
           faster.
